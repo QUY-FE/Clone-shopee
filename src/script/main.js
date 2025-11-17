318a119349b9db0aef84ce2d87ebbd52bc0c5b1b
QUY-FE <quysamp205@gmail.com>
2025-11-17 13:41:01 +0700

    fix bug

--- src/script/main.js
+++ src/script/main.js
@@ -1,24 +1,28 @@
-
-fetch("http://127.0.0.1:5500/public/db.json")
-  .then(response => {
+fetch("https://691ac0762d8d78557570025b.mockapi.io/api/posts/posts")
+  .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     return response.json();
   })
-  .then(data => {
-    
+  .then((data) => {
     const productCard = document.querySelector(".product__card-render");
 
-    data.forEach((product,index) => {
-        productCard.innerHTML += `
+    data.forEach((product, index) => {
+      productCard.innerHTML += `
         <div class="col l-2-4 m-4 c-6">
             <a href="index.html" class="product-item">
-                <div class="product-item__img" style="background-image: url(${product?.imageURL});"></div>
+                <div class="product-item__img" style="background-image: url(${
+                  product?.imageURL
+                });"></div>
                     <h4 class="product-item__heading">${product?.title}</h4>
                     <div class="product-item__price">
-                        <span class="product-item__price-old">₫${product?.oldPrice}</span>
-                        <span class="product-item__price-new">₫${product?.newPrice}</span>
+                        <span class="product-item__price-old">₫${
+                          product?.oldPrice
+                        }</span>
+                        <span class="product-item__price-new">₫${
+                          product?.newPrice
+                        }</span>
                     </div>
                     <!-- has modif -->
                     <!-- product-item__active--liked -->
@@ -34,26 +38,36 @@ fetch("http://127.0.0.1:5500/public/db.json")
                                 <i class="fa-solid fa-star product-item__active-rating--gold-star"></i>
                                 <i class="fa-solid fa-star "></i>
                         </div>
-                        <span class="product-item__sell">Đã bán ${product?.countSell > 1000 ? "1k+" : product?.countSell } </span>
+                        <span class="product-item__sell">Đã bán ${
+                          product?.countSell > 1000 ? "1k+" : product?.countSell
+                        } </span>
                         </div>
                         <div class="product-item__ogirin">
-                            <span class="product-item__ogirin-brand">${product?.brandName} </span>
-                            <span class="product-item__ogirin-name">${product?.countryName}</span>
+                            <span class="product-item__ogirin-brand">${
+                              product?.brandName
+                            } </span>
+                            <span class="product-item__ogirin-name">${
+                              product?.countryName
+                            }</span>
                         </div>
-                        ${product?.favourite === true ? `<div class="product-item__favourite">
+                        ${
+                          product?.favourite === true
+                            ? `<div class="product-item__favourite">
                             <span><i class="fa-solid fa-check"></i>Yêu thích</span>
-                        </div>` : "" }
+                        </div>`
+                            : ""
+                        }
                         <div class="product-item__sale">
                         <span class="product-item__sale-lbl">Giảm</span>
-                        <span class="product-item__sale-percent">${product?.countSell}%</span>
+                        <span class="product-item__sale-percent">${
+                          product?.countSell
+                        }%</span>
                 </div>
             </a>
         </div>
         `;
-        
     });
   })
-  .catch(error => {
-    console.error('Error fetching data:', error); // Handle any errors
+  .catch((error) => {
+    console.error("Error fetching data:", error); // Handle any errors
   });
-
